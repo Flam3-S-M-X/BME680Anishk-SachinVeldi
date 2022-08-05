6831cd7f3adea2f9e8197e0f5997ebe6e8109eae
Flam3-S-M-X <109001669+Flam3-S-M-X@users.noreply.github.com>
2022-08-05 18:13:43 -0400

    Add files via upload

---
+++ bme680test_Final_ANISHK_SACHINVELDI.ino
@@ -0,0 +1,77 @@
+/***************************************************************************
+  This is a library for the BME680 gas, humidity, temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BME680 Breakout
+  ----> http://www.adafruit.com/products/3660
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+const int ledPin = 13;
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME680 bme; // I2C
+//Adafruit_BME680 bme(BME_CS); // hardware SPI
+//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println(F("BME680 test"));
+  pinMode(ledPin,OUTPUT)
+ 
+
+
+  ;if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
+}
+
+void loop() {
+  int delayPeriod = 100;
+  int countDir = 1;
+  if (! bme.performReading()) {
+    Serial.printyln("Failed to perform reading :(");
+    return;
+   static int delayPeriod = 100
+   ;static int countDir = 1;
+   digitalWrite(ledPin ,HIGH);
+   delay(delayPeriod);
+   digitalWrite(ledPin ,LOW);
+   delay(delayPeriod);
+
+   }
+  Serial.print("Gas = ");
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(" KOhms");
+
+ 
+
+  Serial.println();
+  delay(2000);}s
